b7353bd2686296c0883e6d644043a8f2ed8fd19d
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-05-11 16:43:39 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,18 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		@name = "Joe"
+		erb :goodbye
+	end
+
+	get '/date' do
+		@date = Date.today.strftime("%A, %B %d, %Y")
+		erb :date
+	end
 
 end
